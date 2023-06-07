ac0fb48ca721a34cc0f98eee8b95d7260454fb82
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-07 00:06:16 -0300

    Encapsulamento em CSharp

---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Produto produto = new Produto("TV", 500, 10);
+
+            produto.Nome = "T";
+            
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Preco);
+
+
+        }
+    }
+}
